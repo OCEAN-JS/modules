b30ab9cf992d516aa88f50c4e43ad0d711f781cb
RinatValiullov <4rontender@gmail.com>
2018-08-08 01:12:58 +0500

    show ast

--- bundler.js
+++ bundler.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
+const babylon = require('babylon');
+const traverse = require('babel-traverse').default;
+
 
 function createAsset(filename) {
     const content = fs.readFileSync(filename, 'utf-8');
 
-    console.log(content);
+    const ast = babylon.parse(content, {
+        sourceType: "module"
+    });
+
+    const dependencies = [];
+
+    traverse(ast, {
+        ImportDeclaration: ( {node} ) => {
+            dependencies.push(node.source.value);
+        }
+    });
+
+    console.log(dependencies);
 }
 
 createAsset('./my-modules/entry.js');
